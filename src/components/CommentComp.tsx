df0e07b6443d9a8a40a2496557a58acff17e419f
Salomon <salomonleon197@gmail.com>
2023-03-05 22:54:00 -0300

    get time function

--- src/components/CommentComp.tsx
+++ src/components/CommentComp.tsx
@@ -8,6 +8,55 @@ import DeleteModal from "./DeleteModal";
 import plusIcon from "../assets/images/icon-plus.svg";
 import minusIcon from "../assets/images/icon-minus.svg";
 
+function getTime(date: number): string {
+  const seconds = Math.floor(
+    (Date.now() - date) / 1000
+  )
+  if (seconds < 60) {
+    return `${seconds} second${seconds !== 1 ? "s" : ""} ago`
+  } 
+
+  const minutes = Math.floor(
+    seconds / 60
+  )
+  if (minutes < 60) {
+    return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`
+  }
+
+  const hours = Math.floor(
+    minutes / 60
+  )
+  if (hours < 24) {
+    return `${hours} hour${hours !== 1 ? "s" : ""} ago`
+  }
+
+  const days = Math.floor(
+    hours / 24
+  )
+  if (days < 7) {
+    return `${days} day${days !== 1 ? "s" : ""} ago`
+  }
+
+  const weeks = Math.floor(
+    days / 7
+  )
+  if (weeks < 4) {
+    return `${weeks} week${weeks !== 1 ? "s" : ""} ago`
+  }
+
+  const months = Math.floor(
+    days / 30
+  )
+  if (months < 12) {
+    return `${months} month${months !== 1 ? "s" : ""} ago`
+  }
+
+  const years = Math.floor(
+    days / 365
+  )
+  return `${years} year${years !== 1 ? "s" : ""} ago`
+}
+
 type commentProps = {
   commentObj: commentType;
 };
@@ -125,7 +174,7 @@ function CommentComp({ commentObj }: commentProps) {
             <span className="comment__tag">you</span>
           ) : null}
         </address>
-        <time className="comment__date" dateTime="">{commentObj.date}</time>
+        <time className="comment__date" dateTime="">{getTime(commentObj.date)}</time>
       </div>
 
       <div>
